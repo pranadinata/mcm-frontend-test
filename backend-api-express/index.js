75587ec52e5990edf627035508c69aec90aa7f0d
Yesaya Pranadinata <yesayapranadinata.work@gmail.com>
2024-09-01 23:13:04 +0700

    update data

--- backend-api-express/index.js
+++ backend-api-express/index.js
@@ -3,22 +3,14 @@ const cors = require('cors');
 const app = express();
 const axios = require('axios');
 
-app.use(cors()); // Mengizinkan semua domain
-// Atau, mengizinkan hanya domain tertentu
-// app.use(cors({ origin: 'https://yourdomain.com' }));
+app.use(cors());
+
 
 app.get('/api/data', async (req, res) => {
     try {
-        // Melakukan permintaan GET ke API eksternal
-        //   const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
         const response = await axios.get('https://www.mmobomb.com/api1/games?platform=pc');
-
-
-
-        // Mengembalikan data yang diterima dari API ke klien
         res.status(200).json(response.data);
     } catch (error) {
-        // Menangani kesalahan jika permintaan gagal
         res.status(500).json({
             message: 'Error fetching data',
             error: error.message
@@ -28,16 +20,9 @@ app.get('/api/data', async (req, res) => {
 
 app.get('/api/data/:id', async (req, res) => {
     try {
-        // Melakukan permintaan GET ke API eksternal
-        //   const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
         const response = await axios.get('https://www.mmobomb.com/api1/game?id='+req.params.id);
-
-
-        // res.json(req.params.id);
-        // Mengembalikan data yang diterima dari API ke klien
         res.status(200).json(response.data);
     } catch (error) {
-        // Menangani kesalahan jika permintaan gagal
         res.status(500).json({
             message: 'Error fetching data',
             error: error.message
